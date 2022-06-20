dc5ace4a4028a7995ac5ce668fac6f4c0a228387
Not-user-1984 <deman.tmb.68@gmail.com>
2022-06-20 11:07:42 +0300

    New_task

---
+++ Training_task/O_O_P.py
@@ -0,0 +1,30 @@
+
+class Point:
+    color = 'red'
+    circle = 2
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __del__(self):
+        print("удаление " + str(self))
+
+    def set_coords(self, x=0, y=3):
+        self.x = x
+        self.y = y
+
+    def get_coords(self):
+        return self.x, self.y
+
+
+pt = Point(2, 4)
+
+
+print(pt.__dict__)
+
+
+
+
+
+
